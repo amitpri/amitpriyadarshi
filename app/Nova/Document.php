deedba00d12d81c7e64e083d8b9e19b306701c7e
AMit <amitpri@gmail.com>
2020-06-08 17:14:55 +0530

    interview

---
+++ app/Nova/Document.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request; 
+use Laravel\Nova\Fields\Text; 
+use Laravel\Nova\Fields\Trix; 
+use Laravel\Nova\Fields\File;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Document extends Resource
+{
+    public static $group = '0.Plan';
+
+    public static function label() {
+
+        return '4.Documents';
+
+    }
+    public static $model = 'App\Document';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'name' , 'notes'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            Text::make('Name')->sortable()->rules('required', 'max:255'),
+ 
+            File::make('Document','document')->disk('public'), 
+
+            Trix::make('Notes')->alwaysShow()->stacked(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
